53425638d7cb9ee801cf9b6c94c358a6776bf295
VmYs <ysfrank1@gmail.com>
2022-05-18 15:03:46 +0800

    First commit

---
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sunnyweather.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sunnyweather.android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+
+    }
+}
